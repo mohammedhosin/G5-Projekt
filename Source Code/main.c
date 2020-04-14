a7223a9060da894e6bda8185c942eff02c99f6ee
Ernst Reutergårdh <58370642+RTGRDH@users.noreply.github.com>
2020-04-14 19:38:38 +0200

    Gubben kan gå diagonalt samt att man kan trycka in en knapp och den fortsätter gå.

--- Source Code/main.c
+++ Source Code/main.c
@@ -2,12 +2,14 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
 
-const int WIDTH = 960, HEIGTH = 540;
+const int WINDOW_WIDTH = 960, WINDOW_HEIGTH = 540;
 bool init();
 void renderBackground();
 bool initPlayField();
 bool initMedia();
+
 
 
 SDL_Window *window = NULL;
@@ -20,100 +22,166 @@ SDL_Texture *mPlayer = NULL;
 
 SDL_Rect gField;
 SDL_Rect gPlayer;
+#define SPEED (75);
 int main(int argc, const char * argv[])
 {
-    SDL_RendererFlip flip = SDL_FLIP_NONE;
     /**
      Implement into player object?
      Not done yet.
      */
-    SDL_Rect possition;
-    possition.y = 64;
-    possition.x = 110;
-    possition.h = 16;
-    possition.w = 16;
-    int frame = 6;
-    
-    
-    SDL_Event e;
     bool running = true;
     if(init())
     {
-        printf("Initialize successful.");
+        printf("Initialize window and renderer successful.\n");
     }
     //Init backround here
-    if(initPlayField())
+    if(!initPlayField())
     {
-        printf("Initialize play field successful");
+        SDL_FreeSurface(imageSurface);
+        //SDL_FreeSurface(windowSurface);
+        //windowSurface = NULL;
+        imageSurface = NULL;
+        SDL_DestroyWindow(window);
+        SDL_DestroyTexture(mField);
+        SDL_DestroyTexture(mPlayer);
+        SDL_DestroyRenderer(renderer);
+        SDL_Quit();
+        return 1;
+    }
+    else
+    {
+        printf("Initialize playfield successful.\n");
     }
     //Init other graphical media.
-    if(initMedia())
+    if(!initMedia())
     {
-        printf("Initialize media successful");
+        SDL_FreeSurface(imageSurface);
+       //SDL_FreeSurface(windowSurface);
+       //windowSurface = NULL;
+        imageSurface = NULL;
+        SDL_DestroyWindow(window);
+        SDL_DestroyTexture(mField);
+        SDL_DestroyTexture(mPlayer);
+        SDL_DestroyRenderer(renderer);
+        SDL_Quit();
+        return 1;
     }
+    else
+    {
+        printf("Initialize media successful.\n");
+    }
+    /*  Taken from Jonas Willén, SDL_net.zip*/
+        // struct to hold the position and size of the sprite
+        SDL_Rect dest;
+
+        // get and scale the dimensions of texture
+        SDL_QueryTexture(mPlayer, NULL, NULL, &dest.w, &dest.h);
+        dest.w /= 10;
+        dest.h /= 10;
+
+        // start sprite in the the middle of the goal
+        //float x_pos = (WINDOW_WIDTH - dest.w); //Starting x-pos for right team
+        float x_pos = (0); //Starting x-pos for left team
+        float y_pos = (WINDOW_HEIGTH - dest.h) / 2;
+        float x_vel = 0;
+        float y_vel = 0;
+
+        // keep track of which inputs are given
+        int up = 0;
+        int down = 0;
+        int left = 0;
+        int right = 0;
+        
+    /*  End of SDL_net.zip code*/
     while(running)
     {
         /**
         While loop checking if an event occured.
+         Code taken from Jonas Willén, SDL_net.zip
          */
-        while (SDL_PollEvent(&e))
-        {
-            if (e.type == SDL_QUIT)
+            SDL_Event event;
+            while (SDL_PollEvent(&event))
             {
-                running = false;
-            }
-
-            else if( e.type == SDL_KEYDOWN )
-            {
-                //Select surfaces based on key press
-                switch( e.key.keysym.sym )
+                switch (event.type)
                 {
-                    case SDLK_UP:
-                        possition.y -= 2;
-                        flip = SDL_FLIP_NONE;
-                        if(frame == 4)
-                            frame = 5;
-                        else
-                            frame = 4;
+                case SDL_QUIT:
+                    running = false;
+                    break;
+                case SDL_KEYDOWN:
+                    switch (event.key.keysym.scancode)
+                    {
+                    case SDL_SCANCODE_W:
+                    case SDL_SCANCODE_UP:
+                        up = 1;
                         break;
-                    case SDLK_DOWN:
-                        possition.y += 2;
-                        flip = SDL_FLIP_NONE;
-                        if(frame == 0)
-                            frame = 1;
-                        else
-                            frame = 0;
+                    case SDL_SCANCODE_A:
+                    case SDL_SCANCODE_LEFT:
+                        left = 1;
                         break;
-                    case SDLK_LEFT:
-                        possition.x -= 2;
-                        flip = SDL_FLIP_HORIZONTAL;
-                        if(frame == 2)
-                            frame = 3;
-                        else
-                            frame = 2;
+                    case SDL_SCANCODE_S:
+                    case SDL_SCANCODE_DOWN:
+                        down = 1;
                         break;
-                    case SDLK_RIGHT:
-                        possition.x += 2;
-                        flip = SDL_FLIP_NONE;
-                        if(frame == 2)
-                            frame = 3;
-                        else
-                            frame = 2;
+                    case SDL_SCANCODE_D:
+                    case SDL_SCANCODE_RIGHT:
+                        right = 1;
                         break;
-                    default:
-                        
+                    }
+                    break;
+                case SDL_KEYUP:
+                    switch (event.key.keysym.scancode)
+                    {
+                    case SDL_SCANCODE_W:
+                    case SDL_SCANCODE_UP:
+                        up = 0;
                         break;
+                    case SDL_SCANCODE_A:
+                    case SDL_SCANCODE_LEFT:
+                        left = 0;
+                        break;
+                    case SDL_SCANCODE_S:
+                    case SDL_SCANCODE_DOWN:
+                        down = 0;
+                        break;
+                    case SDL_SCANCODE_D:
+                    case SDL_SCANCODE_RIGHT:
+                        right = 0;
+                        break;
+                    }
+                    break;
                 }
-            }
-            
     }
-       
         
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        /* Taken from Jonas Willén, SDL_net.zip*/
+       // determine velocity
+       x_vel = y_vel = 0;
+       if (up && !down) y_vel = -SPEED;
+       if (down && !up) y_vel = SPEED;
+       if (left && !right) x_vel = -SPEED;
+       if (right && !left) x_vel = SPEED;
+
+       // update positions
+       x_pos += x_vel / 60;
+       y_pos += y_vel / 60;
+
+       // collision detection with bounds
+       if (x_pos <= 0) x_pos = 0;
+       if (y_pos <= 0) y_pos = 0;
+       if (x_pos >= WINDOW_WIDTH - dest.w) x_pos = WINDOW_WIDTH - dest.w;
+       if (y_pos >= WINDOW_HEIGTH - dest.h) y_pos = WINDOW_HEIGTH - dest.h;
+        
+       // set the positions in the struct
+       dest.y = (int) y_pos;
+       dest.x = (int) x_pos;
+        /*  end of SDL_net.zip*/
+        
         SDL_RenderClear(renderer);
         renderBackground();
-        SDL_RenderCopyEx(renderer, mPlayer, &gPlayer, &possition, 0, NULL, SDL_FLIP_NONE);
+        SDL_RenderCopyEx(renderer, mPlayer, &gPlayer, &dest, 0, NULL, SDL_FLIP_NONE);
         SDL_RenderPresent(renderer);
+        
+        
+        SDL_Delay(1000/50);
     }
     SDL_FreeSurface(imageSurface);
     //SDL_FreeSurface(windowSurface);
@@ -121,6 +189,8 @@ int main(int argc, const char * argv[])
     imageSurface = NULL;
     SDL_DestroyWindow(window);
     SDL_DestroyTexture(mField);
+    SDL_DestroyTexture(mPlayer);
+    SDL_DestroyRenderer(renderer);
     SDL_Quit();
     return 0;
 }
@@ -164,7 +234,7 @@ bool initPlayField()
     imageSurface = IMG_Load("images/SoccerField.png");
     
     mField = SDL_CreateTextureFromSurface(renderer, imageSurface);
-    gField.x = 0; gField.y = 0; gField.h = HEIGTH; gField.w = WIDTH;
+    gField.x = 0; gField.y = 0; gField.h = WINDOW_HEIGTH; gField.w = WINDOW_WIDTH;
     if(NULL == imageSurface)
     {
         printf("\nCould not load image. Error: %s",SDL_GetError());
@@ -195,7 +265,7 @@ bool init()
 {
     bool test = true;
     SDL_Init(SDL_INIT_VIDEO);
-    window = SDL_CreateWindow("Under production", SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGTH, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("Under production", SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGTH, SDL_WINDOW_SHOWN);
     
     //windowSurface = SDL_GetWindowSurface(window);
     
